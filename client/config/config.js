af837ff1868f4b2ff6e8547c1047ec09a2bfc692
Muhammad Umair <muhammadumair7211@gmail.com>
2026-08-25 19:29:53 +0500

    ...

--- client/config/config.js
+++ client/config/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = "https://api.mercadolibreonline.id/api";
-export const imageUrl = "https://api.mercadolibreonline.id";
+export const baseUrl = "https://api.mcshop99.com/api";
+export const imageUrl = "https://api.mcshop99.com";
